45fe1e86e0783b76486f365e40b224e4e5605ee7
JustGreen <sommarter@yandex.ru>
2022-08-05 04:29:06 +0600

    убрано ненужное условие

--- dz1-2.py
+++ dz1-2.py
@@ -2,11 +2,6 @@ result = int(input('Введите время в секундах:'))
 hour = result // 3600
 min = (result - hour * 3600) // 60
 with_min = hour * 3600 + min * 60
-
-if result > with_min:
-    sec = result - with_min
-else:
-    sec = 00
-
-print(f'{result} это {hour}ч :{min}м :{sec}с')
+sec = result - with_min
+print(f'{result} секунд это {hour}ч :{min}м :{sec}с')
 
